881c3d71beaef46c255edf9b85fbbbb36412cf6f
qw0ntum <qw0ntum@4ba16a4c-0c44-0410-b45d-853d675659d1>
2008-03-19 13:13:58 +0000

    Updated headers, login, stub for network config.

--- trunk/orangeMesh/create.php
+++ trunk/orangeMesh/create.php
@@ -2,25 +2,27 @@
 /* Name: create.php
  * Purpose: create a new network in the dashboard.
  * Written By: Shaddi Hasan
- * Last Modified: March 13, 2008 12.30pm
+ * Last Modified: March 16, 2008
  * 
  * (c) 2008 Orange Networking.
  *  
  * This file is part of OrangeMesh.
  *
  * OrangeMesh is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * (at your option) any later version. This license is similar to the GNU
+ * General Public license, but also requires that if you extend this code and
+ * use it on a publicly accessible server, you must make available the 
+ * complete source source code, including your extensions.
  *
  * OrangeMesh is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with OrangeMesh.  If not, see <http://www.gnu.org/licenses/>.
- * 
  */
 
 	if(!isset($_POST['create'])){
@@ -35,22 +37,38 @@
 </head>
 <body>
 	<form method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>" onsubmit="if(!isFormValid()){
-			alert('The fields highlighted in red have errors. Please correct this and resubmit.');return false;}"name="createNetwork">Please fill in the following
-	information to register a new network.<br>
-	  <br>
-	Network Name <input name="acct_name" required="1"><br>
-	  <br>
-	Password <input name="password" required="1" type="password"><br>
-	  <br>
-	Primary email address <input name="email1" required="1" mask="email"><br>
-	  <br>
-	Network Location <input name="net_location" required="1" freemask="NNNNN" maxlength="5"><br>
-	  <br>
-	Email for notifications <input name="email2" default=" "><br>
-	  <br>
-	  <input name="create" value="Create Network" type="submit"> <input name="reset" value="Reset" type="reset"><br>
+			alert('The fields highlighted in red have errors. Please correct this and resubmit.');return false;}"name="createNetwork">
+	Please fill in the following information to register a new network. Fields outlined in red have errors.
+	<table>
+		<tr>
+			<td><font color="red">*</font> Network Name (no spaces, please)</td>
+			<td><input name="net_name" required="1"></td>
+		</tr>
+		<tr>
+			<td><font color="red">*</font> Password </td>
+			<td><input name="password" required="1" type="password"></td>
+		</tr>
+		<tr>
+			<td><font color="red">*</font> Primary email address </td>
+			<td><input name="email1" required="1" mask="email"></td>
+		</tr>
+		<tr>
+			<td><font color="red">*</font> Network Location (5-digit zipcode)</td>
+			<td><input name="net_location" required="1" freemask="NNNNN" maxlength="5"></td>
+		</tr>
+		<tr>
+			<td>Email for notifications</td>
+			<td><input name="email2" default=" "></td>
+		</tr>
+		<tr>
+			<td><input name="create" value="Create Network" type="submit"></td>
+			<td><input name="reset" value="Reset" type="reset"></td>
+		</tr>
+		<tr>
+			<td><font color="red">* Required Field</font></td>
+		</tr>
+	</table>
 	</form>
-	<br>
 	Problem? Check the <a href="help/help.php">help,</a> then <a href="mailto:shasan@email.unc.edu">notify us.</a>
 	</body>
 </html>
@@ -89,18 +107,16 @@
 			return $values;
 		}
 		
-		//define the database configuration stuff
-		$dbHost = "localhost";
-		$dbUser = "orangemesh";
-		$dbPass = "default";
-		$dbName = "orangemesh";
-		$dbTable = "accounts";
 		
-		//create connection to db
-		$conn = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName) or die("Error connecting to database: ".$dbHost);
+		//setup database connection
+		require 'connectDB.php';
+		setTable('network');
 		
 		//the fields we want to insert into the database
-		$fields = array("acct_name","password","email1","net_location","email2");
+		$fields = array("net_name","password","email1","net_location","email2");
+		
+		//hash the input password
+		$_POST["password"] = md5($_POST["password"]);
 		
 		//get the values corresponding to the above fields from the user input 
 		$values = getValuesFromPOST($fields);
