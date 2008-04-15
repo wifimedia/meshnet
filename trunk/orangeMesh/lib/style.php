3c29047b29989137ed48d50c96025b168e8def9a
qw0ntum <qw0ntum@4ba16a4c-0c44-0410-b45d-853d675659d1>
2008-04-15 23:29:10 +0000

    updated edit settings page, nifty corners, update style sheet

--- trunk/orangeMesh/lib/style.php
+++ trunk/orangeMesh/lib/style.php
@@ -2,3 +2,4 @@
 $on_index = (boolean)strpos($_SERVER['PHP_SELF'],'index.php');
 ?>
 <LINK REL=STYLESHEET HREF="<?if(!$on_index){echo '../';}?>lib/style.css" TYPE="text/css">
+<script type=text/javascript src="<?if(!$on_index){echo '../';}?>lib/niftycube.js"></script>
