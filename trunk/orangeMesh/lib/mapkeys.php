4565dba351bc48a26c0f5bc8a7290164b3af9e0d
qw0ntum <qw0ntum@4ba16a4c-0c44-0410-b45d-853d675659d1>
2008-04-17 02:45:43 +0000

    updated install doc for maps, added fields to ease adding map keys

--- trunk/orangeMesh/lib/mapkeys.php
+++ trunk/orangeMesh/lib/mapkeys.php
@@ -24,17 +24,27 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with OrangeMesh.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+//just edit these two lines!
+$myAddress = "your.address.here";	//the address you type into your browser to get to your orangemesh server, without the "http://"
+$myKey = "your google map key goes here";	//see http://code.google.com/apis/maps/signup.html for a key
+
+
 $host = $_SERVER['HTTP_HOST'];
+
+
 
 //
 // generate your google map API keys (http://code.google.com/apis/maps/index.html) and paste them here.
 // you'll need two:  One for your domain with the www and one without.
 //
 
-if ($host == "localhost")  // change to www.your-domain.com
-  echo '<script src="http://maps.google.com/maps?file=api&v=2&key=ABQIAAAAOblURTDowPDJJzfEXmiWIBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQbL_rhHeHuGFVXrVCUAJB7anr10w" type="text/javascript"></script>'."\n" ;
-else if ($host == "omnis.hopto.org")  // change to www.your-domain.com
-  echo '<script src="http://maps.google.com/maps?file=api&v=2&key=ABQIAAAAOblURTDowPDJJzfEXmiWIBTZjxrB_pn9jGmMh-RwF7AKb9MO5BSCsu6kbvCWascCDdDG4x87WRQixA" type="text/javascript"></script>'."\n" ;
-else if ($host == "open-mesh.com") // change to your-domain.com (w/o www)
-  echo '<script src="http://maps.google.com/maps?file=api&v=2&key=ABQIAAAAOblURTDowPDJJzfEXmiWIBTZjxrB_pn9jGmMh-RwF7AKb9MO5BSCsu6kbvCWascCDdDG4x87WRQixA" type="text/javascript"></script>'."\n" ;
+if ($host == "localhost")
+	echo '<script src="http://maps.google.com/maps?file=api&v=2&key=ABQIAAAAOblURTDowPDJJzfEXmiWIBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQbL_rhHeHuGFVXrVCUAJB7anr10w" type="text/javascript"></script>'."\n" ;
+else if ($host == "omnis.hopto.org")
+	echo '<script src="http://maps.google.com/maps?file=api&v=2&key=ABQIAAAAOblURTDowPDJJzfEXmiWIBTZjxrB_pn9jGmMh-RwF7AKb9MO5BSCsu6kbvCWascCDdDG4x87WRQixA" type="text/javascript"></script>'."\n" ;
+else if ($host == "open-mesh.com")
+	echo '<script src="http://maps.google.com/maps?file=api&v=2&key=ABQIAAAAOblURTDowPDJJzfEXmiWIBTZjxrB_pn9jGmMh-RwF7AKb9MO5BSCsu6kbvCWascCDdDG4x87WRQixA" type="text/javascript"></script>'."\n" ;
+else if ($host == $myAddress)
+	echo '<script src="http://maps.google.com/maps?file=api&v=2&key='.$myKey.'" type="text/javascript"></script>'."\n" ;  
 ?>
